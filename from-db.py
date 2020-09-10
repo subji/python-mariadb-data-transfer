e23403bd9424c6df0e555f6e9166f983b73e9f6f
pjs <pjs@gitlab.daumsoft.com>
2020-09-10 20:06:34 +0900

    update

--- from-db.py
+++ from-db.py
@@ -6,11 +6,19 @@ import json
 import mysql.connector
 
 config = {
-  
+  'user': 'sometrend_prod',
+  'password': 'daumsoft0531!', 
+  'host': '10.1.51.35', 
+  'database': 'sometrend_prod', 
+  'port': '3306'
 }
 
 to_config = {
-  
+  'user': 'sometrend_analysis_prod',
+  'password': 'sometrend_analysis_prod@2020', 
+  'host': '10.1.51.35', 
+  'database': 'sometrend_analysis_prod', 
+  'port': '3306'
 }
 
 conn = mysql.connector.connect(**config)
@@ -35,46 +43,68 @@ def mergeMany(query, values, buffered=True):
     to_conn.rollback()
     raise e
 
-try:
-  # === t_trend_analysis -> t_trend_analysis_info
-  # select_query = 'SELECT * FROM T_TREND_ANALYSIS'
+def makeAnalysisInfoSeq():
+  select_query = 'SELECT * FROM T_TREND_ANALYSIS'
 
-  # analysis_json = {}
-  # to_analysis_data = []
+  analysis_json = {}
 
-  # with open('./user_id_with_random_uuid.json') as user_json, open('analysis_id_with_random_uuid.json') as analysis_json:
-  #   user = json.load(user_json)
-  #   analysis = json.load(analysis_json)
+  for row in select(select_query):
+    while True:
+      uuid_str = str(uuid.uuid4())
 
-  #   for row in select(select_query):
-  #     if user.get(str(row[1])) != None:
-  #       start_date = row[6][0:4] + '.' + row[6][4:6] + '.' + row[6][6:]
-  #       end_date = row[7][0:4] + '.' + row[7][4:6] + '.' + row[7][6:]
-  #       insert_data = (analysis[str(row[0])], user[str(row[1])], row[2], ('keyword' if row[3] == 'issue' else row[3]), row[4], row[5], start_date, end_date, row[8], row[13], row[9], row[10], row[11], row[12])
-  #       to_analysis_data.append(insert_data)
-  #   # while True:
-  #   #   uuid_str = str(uuid.uuid4())
+      if analysis_json.get(uuid_str) == None:
+        analysis_json[uuid_str] = str(row[0])
+        break
 
-  #   #   if analysis_json.get(uuid_str) == None:
-  #   #     analysis_json[uuid_str] = str(row[0])
-  #   #     break
+  analysis_json = dict((v, k) for k, v in analysis_json.items())
 
-  # # analysis_json = dict((v, k) for k, v in analysis_json.items())
+  with open('analysis_id_with_random_uuid.json', 'w') as json_file:
+    json.dump(analysis_json, json_file)
 
-  # # with open('analysis_id_with_random_uuid.json', 'w',) as json_file:
-  # #   json.dump(analysis_json, json_file)
-  # print(to_analysis_data)
-  # insert_query = """
-  #   INSERT INTO t_trend_analysis_info 
-  #   (trend_analysis_info_seq, user_seq, menu_type, analysis_type, search_keyword, compare_keyword, analysis_start_date, analysis_end_date, analysis_title, is_mobile, del_yn, register_date, update_date, delete_date) 
-  #   VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-  # """
-  # mergeMany(insert_query, to_analysis_data)
-
-  # t_trend_analysi_file -> t_trend_analysis_file
+def makeAnalysisFileSeq():
   select_query = 'SELECT * FROM t_trend_analysis_file'
 
-  # file_json_data = {}
+  file_json_data = {}
+
+  for row in select(select_query):
+    while True:
+      uuid_str = str(uuid.uuid4())
+
+      if file_json_data.get(uuid_str) == None:
+        file_json_data[uuid_str] = str(row[0])
+        break
+
+  file_json_data = dict((v, k) for k, v in file_json_data.items())
+
+  with open('analysis_file_id_with_random_uuid.json', 'w') as json_file:
+    json.dump(file_json_data, json_file)
+
+def insertAllAnalysis():
+  select_query = 'SELECT * FROM T_TREND_ANALYSIS'
+  to_analysis_data = []
+
+  with open('user_id_with_random_uuid.json') as user_json, open('analysis_id_with_random_uuid.json') as analysis_json:
+    user = json.load(user_json)
+    analysis = json.load(analysis_json)
+
+    for row in select(select_query):
+      if user.get(str(row[1])) != None:
+        start_date = row[6][0:4] + '.' + row[6][4:6] + '.' + row[6][6:]
+        end_date = row[7][0:4] + '.' + row[7][4:6] + '.' + row[7][6:]
+        insert_data = (analysis[str(row[0])], user[str(row[1])], row[2], ('keyword' if row[3] == 'issue' else row[3]), row[4], row[5], start_date, end_date, row[8], row[13], row[9], row[10], row[11], row[12])
+        to_analysis_data.append(insert_data)
+
+  insert_query = """
+     INSERT INTO t_trend_analysis_info 
+     (trend_analysis_info_seq, user_seq, menu_type, analysis_type, search_keyword, compare_keyword, analysis_start_date, analysis_end_date, analysis_title, is_mobile, del_yn, register_date, update_date, delete_date) 
+     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+   """
+
+  mergeMany(insert_query, to_analysis_data)
+
+def insertAllAnalysisFile():
+  select_query = 'SELECT * FROM t_trend_analysis_file'
+
   insert_data = []
   url_path = 'https://static.some.co.kr/sometrend/attachments/analysis/'
   real_path = '/app_nfs/sometrend/analysis/'
@@ -122,26 +152,19 @@ try:
 
         data = (file_uid[str(row[0])], analysis[str(row[1])], new_url_path, new_real_path, row[4], row[5], row[6], row[7], row[8])
         insert_data.append(data)
-      # print (data)
-      # while True:
-      #   uuid_str = str(uuid.uuid4())
-
-      #   if file_json_data.get(uuid_str) == None:
-      #     file_json_data[uuid_str] = str(row[0])
-      #     break
 
   insert_query = """INSERT INTO t_trend_analysis_file 
     (trend_analysis_file_seq, trend_analysis_info_seq, file_url_path, file_real_path, file_name, del_yn, register_date, update_date, delete_date) 
     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)
   """
 
-  # mergeMany(insert_query, insert_data)
-  
-  # file_json_data = dict((v, k) for k, v in file_json_data.items())
+  mergeMany(insert_query, insert_data)
 
-  # with open('analysis_file_id_with_random_uuid.json', 'w',) as json_file:
-  #   json.dump(file_json_data, json_file)
-
+try:
+  # makeAnalysisInfoSeq()
+  # makeAnalysisFileSeq()
+  # insertAllAnalysis()
+  insertAllAnalysisFile()
 except Exception as e:
   print(e)
 finally:
